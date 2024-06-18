c8c183c4b93ccdcf4fd40f9bf3fee85d281c1ca0
aiueo-1234 <130837816+aiueo-1234@users.noreply.github.com>
2024-06-18 13:15:53 +0900

    例外処理の追加

--- PortAudioCSharp/Exceptions/PortAudioException.cs
+++ PortAudioCSharp/Exceptions/PortAudioException.cs
@@ -11,14 +11,25 @@ public class PortAudioException : Exception
     public PortAudioException(string message, Exception inner) : base(message, inner) { }
     public PortAudioException(PaErrorCode paErrorCode) : base(PortAudioWrapper.GetErrorText(paErrorCode)) { }
 
-    public static void ThrowIfError(PaErrorCode paErrorCode){
-        if(paErrorCode!=PaErrorCode.paNoError){
+    public static void ThrowIfError(PaErrorCode paErrorCode)
+    {
+        if (paErrorCode != PaErrorCode.paNoError)
+        {
             throw new PortAudioException(paErrorCode);
         }
     }
 
+    public static void ThrowIfError(int value)
+    {
+        if (Enum.IsDefined(typeof(PaErrorCode), value))
+        {
+            ThrowIfError((PaErrorCode)value);
+        }
+    }
+
     [DoesNotReturn]
-    public static void Throw(PaErrorCode paErrorCode){
+    public static void Throw(PaErrorCode paErrorCode)
+    {
         throw new PortAudioException(paErrorCode);
     }
 }
