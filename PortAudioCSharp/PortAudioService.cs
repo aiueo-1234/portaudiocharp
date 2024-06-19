32b16a87b479308a44c6af601cffc2a656553c0e
aiueo-1234 <130837816+aiueo-1234@users.noreply.github.com>
2024-06-19 09:34:41 +0900

    Serviceの追加

---
+++ PortAudioCSharp/PortAudioService.cs
@@ -0,0 +1,36 @@
+﻿using PortAudio.Exceptions;
+using PortAudio.Wrapper;
+
+namespace PortAudioCSharp;
+
+public sealed class PortAudioService : IDisposable
+{
+    private bool _isInitialized = false;
+
+    public bool IsInitialized
+    {
+        get
+        {
+            if (_isInitialized)
+            {
+                Initialize();
+            }
+            return _isInitialized;
+        }
+    }
+
+    internal void Initialize()
+    {
+        PortAudioException.ThrowIfError(PortAudioWrapper.Initialize());
+        _isInitialized = true;
+    }
+
+    public void Dispose()
+    {
+        if (_isInitialized)
+        {
+            PortAudioException.ThrowIfError(PortAudioWrapper.Terminate());
+        }
+        _isInitialized = false;
+    }
+}
